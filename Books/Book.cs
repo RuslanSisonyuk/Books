bb684f380fdb21e5f208c526eca153bb0782f3a4
rRavsh <105485658+rRavsh@users.noreply.github.com>
2023-02-09 17:13:23 +0200

    Reworked the quotes function and added properties to each class variables

--- Books/Book.cs
+++ Books/Book.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Books
 {
@@ -13,35 +15,51 @@ namespace Books
         
         public Book(int pages, string auth, string contents, string publisher)
         {
-            this.number_of_pages = pages;
-            this.author = auth;
-            this.publisher = publisher;
-            this.contents = contents;
+            this.Pages = pages;
+            this.Author = auth;
+            this.Publisher = publisher;
+            this.Contents = contents;
         }
-
         private int Pages
         {
             get { return number_of_pages; }
             //give an error when this happens?
             set { number_of_pages = number_of_pages < 0 ? 0 : number_of_pages; }
         }
-
         private string Author 
         {
             get { return author; }
-            set { }
+            set {
+                string r = @"^\w+\s+\w*$";
+                author = Regex.IsMatch(author,r) ? author : "error";
+            }
+        }
+        private string Contents
+        {
+            get { return contents; }
+            set
+            {
+                contents = contents.Length > 1000 ? "Given text is too long" : contents;
+            }
+        }
+        private string Publisher
+        {
+            get { return publisher; }
+            set
+            {
+                string r = @"^\w+\s+\w*$";
+                publisher = Regex.IsMatch(publisher, r) ? publisher : "error";
+            }
         }
     }
 
     class Comic : Book
     {
         protected int panels_per_page;
-
         public Comic(int panels, int pages, string auth, string contents, string publisher) : base(pages,auth,contents,publisher)
         {
             this.Panels_per_page = panels;
         }
-
         private int Panels_per_page
         {
             get { return panels_per_page; }
@@ -56,21 +74,22 @@ namespace Books
     {
         public Newspaper(int pages, string auth, string contents, string publisher) : base(pages, auth, contents, publisher) 
         { }
-
         //rework to get the quotes from a file
         public void Use_as_intended()
         {
-            List<string> quotes = new List<string>();
-            quotes.Add("GET THIS YOU PESKY FLY!");
+            string[] str = System.IO.File.ReadAllLines("/Contents/quotes.txt");
+            List<string> quotes = new List<string>(str);
+
+            /*quotes.Add("GET THIS YOU PESKY FLY!");
             quotes.Add("IT'S NO USE!");
             quotes.Add("Crap, I'm out of paper...");
             quotes.Add("*Reads about brain transplant* \n `Where did you get that brain...  The brain store?! ... \n " +
                 "*The room falls in complete fucking silence while you, feeling everybody's judging gazes, reconsider your life choises*");
-            quotes.Add("You've chosen your weapon well.");
+            quotes.Add("You've chosen your weapon well.");*/
+
             var random = new Random();
             int quote_id = random.Next(quotes.Count);
             Console.WriteLine(quotes[quote_id]);
         }
     }
-
 }
---
+++ Books/BookStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace Books
+{
+    class BookStore
+    {
+        //menu
+        //
+
+        //list books
+        //access info about the book
+        //read book
+        //sort books:
+        //-by name
+        //-by category
+        List<Book> Get_all_books(string filename)
+        {
+            
+            string json_string = File.ReadAllText(filename+".json");
+            List<Book> books = JsonSerializer.Deserialize<List<Book>>(json_string);
+            return books;
+        }
+
+
+        static void Main(string[] args)
+        {
+               //read the books info from json? file
+               
+        }
+    }
+}
