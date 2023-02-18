0d6eeffa88be284db0be9fcd7d98e64ddb177a7b
rRavsh <105485658+rRavsh@users.noreply.github.com>
2023-02-18 17:29:57 +0200

    Handled exceptions on menu options, added routing to option functions (read, modify, delete entry from list)

--- Books/BookStore.cs
+++ Books/BookStore.cs
@@ -8,6 +8,13 @@ namespace Books
 {
     class Utils
     {
+        static private void Print_exception(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.ReadLine();
+            System.Environment.Exit(1);
+        }
         static public List<Dictionary<string, string>> Deserialize_books_json(string filename)
         {
             string json_string = File.ReadAllText(@"Contents\" + filename + ".json");
@@ -15,70 +22,98 @@ namespace Books
         }
         static public List<Book> Get_all_books(string filename)
         {
-            var json_list = Deserialize_books_json(filename);
             List<Book> books = new List<Book>();
-            foreach (var dict in json_list)
+            try
             {
-                Book book = new Book(dict["name"], Int32.Parse(dict["pages"]), dict["author"], dict["publisher"], dict["content"]);
-                books.Add(book);
+                var json_list = Deserialize_books_json(filename);
+                foreach (var dict in json_list)
+                {
+                    Book book = new Book(dict["name"], Int32.Parse(dict["pages"]), dict["author"], dict["publisher"], dict["content"]);
+                    books.Add(book);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Print_exception("File not found");
+            }
+            catch(Exception ex)
+            {
+                Print_exception(ex.Message);
             }
             return books;
         }
-
-        
     }
     class BookStore
     {
-        //menu
-        //read book
-        //sort books:
-        //-by name
-        //-by category
-
-        static public void exception_page(int exception, List<Book> books)
+        static public void Return_to_menu(List<Book> books)
+        {
+            Console.WriteLine("Press esc to return to main screen");
+            while (!(Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape))
+            { }
+            Book_menu(books);
+        }
+        static public void exception_page(Exception exception, List<Book> books)
         {
             Console.Clear();
             Console.WriteLine("Caught an exception: " + exception);
-            Console.WriteLine("Press 1 to return to main screen");
-            while (!(Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape))
-            {}
-            Book_menu(books);   
+            Return_to_menu(books);
+        }
+        static public void Read_book(List<Book> books, int book_id)
+        {
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine(books[book_id].Contents);
+            Console.WriteLine("-------------------------------------------");
+            Return_to_menu(books);
+        }
+        static public void Modify_book(List<Book> books, int book_id)
+        {
+            Return_to_menu(books);
+        }
+        static public void Delete_book(List<Book> books, int book_id)
+        {
+            Return_to_menu(books);
         }
         static public void Route_to_option(List<Book> books, int book_id, int option)
         {
-
+            Console.Clear();
+            switch(option)
+            {
+                case 1: Read_book(books, book_id); break;
+                case 2: Modify_book(books, book_id); break;
+                case 3: Delete_book(books, book_id); break;
+                default: break;
+            }
         }
         static public void Book_menu(List<Book> books)
         {
             Console.Clear();
             Console.WriteLine("-------------------------------------------");
             int i = 1;
-            foreach (var book in books)
-            {
-                Console.WriteLine(i + " | " + book.Print_book_info());
-                i++;
+            foreach (var book in books) 
+            { 
+                Console.WriteLine(i + " | " + book.Print_book_info()); i++; 
             }
-            Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("Select the book by its number");
-            var selected_book = Console.ReadLine();
             try
             {
-                int sb = Int32.Parse(selected_book);
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Select the book by its number");
+                int book_id = Int32.Parse(Console.ReadLine());
+                if (book_id > books.Count || book_id < 1) { Book_menu(books); return; }
+                Console.WriteLine("Select:   1-Read book  |  2-Change its information  |  3-Delete book");
+                int option = Int32.Parse(Console.ReadLine());
+                if (option > 3 || option < 0) { Book_menu(books); return; }
+                Route_to_option(books, book_id-1, option);
             }
-            catch
+            catch(FormatException exception)
             {
-
+                exception_page(exception, books);
             }
-            Console.WriteLine("Select:   1-Read book  |  2-Change its information  |  3-Delete book");
-            var option = Console.ReadLine();
-            Route_to_option(books, Int32.Parse(selected_book), Int32.Parse(option));
         }
 
         static void Main(string[] args)
         {
             List<Book> books = Utils.Get_all_books("books");
-            exception_page(1, books);
-            //Book_menu(books);
+            Book_menu(books);
         }
     }
 }
